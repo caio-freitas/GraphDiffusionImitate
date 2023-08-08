09b48d7e31466d80fe7b1638642bd9c83cc46888
Caio Freitas <caio.freitas@usp.br>
2023-08-08 18:08:03 +0200

    se2_state_dataset, training and testing of mlp

--- imitation/env/pybullet/se2_envs/robot_se2_wrapper.py
+++ imitation/env/pybullet/se2_envs/robot_se2_wrapper.py
@@ -25,13 +25,17 @@ class DifferentiableSE2(DifferentiableTree):
 
 class RobotSe2EnvWrapper(gym.Env):
     def __init__(self,
-                 num_obs=2,):
+                 num_obs=2,
+                 start_pose=[0,0,-1],
+                 start_quat=[0,0,0,1]):
         self._generate_obstacle_spheres(num_obs)
         self.env = SE2BotPickPlace(objects_list=['cube' for i in range((self.obstacle_spheres.shape[1]))],
                           obj_poses=[[self.obstacle_spheres[0][i,:3], [0,0,0,1]] for i in range(self.obstacle_spheres.shape[1])])
         
         self.N_DOF = self.env.dof
-        
+        device = torch.device('cpu')
+        tensor_args = {'device': device, 'dtype': torch.float32}
+
         self.action_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.N_DOF,), dtype=np.float32)
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.N_DOF,), dtype=np.float32) # TODO what else for observation space?
 
@@ -43,7 +47,16 @@ class RobotSe2EnvWrapper(gym.Env):
         self.robot_fk = DifferentiableSE2(device='cpu')
 
         self.state = self.env.getJointStates()[0]
-        self.env.step(self.state)
+
+        # start_pose = torch.tensor(start_pose, **tensor_args)
+        # start_quat = torch.tensor(start_quat, **tensor_args)
+        # start_joints = p.calculateInverseKinematics(self.env,
+        #                                         self.env.JOINT_ID[-1],
+        #                                         start_pose, 
+        #                                         start_quat)[:self.env.dof]
+        # self.start_joints = torch.tensor(start_joints, **tensor_args)
+    
+        # self.env.step(self.start_joints)
 
 
 
@@ -59,8 +72,8 @@ class RobotSe2EnvWrapper(gym.Env):
                 self.obstacle_spheres[0, i, 3] = r
 
     def reset(self):
-        obs = self.env.reset()
-        return obs
+        [robot, obstacles, grasp_obj] = self.env.reset() # TODO use start_joints
+        return robot
 
     def step(self, action):
         [robot, obstacles, grasp_obj] = self.env.step(action)
@@ -71,8 +84,8 @@ class RobotSe2EnvWrapper(gym.Env):
         - info
         - observation
         '''
-        time.sleep(0.01)
-        return [robot, obstacles, grasp_obj]
+        time.sleep(0.05)
+        return robot # pose only
 
     def render(self, mode="human"):
         pass
