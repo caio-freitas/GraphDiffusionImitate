eb21c303fc7cf6a5a8b9d4c401689f13ff69dbd4
Caio Freitas <caio.freitas@usp.br>
2023-08-08 16:21:04 +0200

    update Se2Agent

--- imitation/agent/robot_se2_agent.py
+++ imitation/agent/robot_se2_agent.py
@@ -3,7 +3,7 @@ from imitation.agent.base_agent import BaseAgent
 class Se2Agent(BaseAgent):
     def __init__(self, policy):
         super().__init__(policy)
-
+        self.env = policy.env
     def act(self, observation):
         # TODO should act on environment
         return self.policy.get_action(observation)
