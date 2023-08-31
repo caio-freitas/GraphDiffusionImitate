63de900aaef8f4541ff7f49a780190b38a925091
Caio Freitas <caio.freitas@usp.br>
2023-08-31 18:55:38 +0200

    fix gym kitchen wrapper

--- imitation/env/kitchen_pose/gym_kitchen_wrappers.py
+++ imitation/env/kitchen_pose/gym_kitchen_wrappers.py
@@ -1,4 +1,5 @@
-import gymnasium as gym
+import gymnasium
+import gym
 
 class KitchenPoseWrapper(gym.Wrapper):
     '''
@@ -6,29 +7,35 @@ class KitchenPoseWrapper(gym.Wrapper):
     '''
     def __init__(self):
         self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(10,))
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(7,))
-        self.env = gym.make(
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(9,))
+        self.env = gymnasium.make(
             'FrankaKitchen-v1',
-            tasks_to_complete=['microwave', 'kettle'],
+            tasks_to_complete=['microwave'],
             render_mode='human'
-            )
+        )
 
     def step(self, action):
-        obs, reward, done, info = self.env.step(action)
+        all = self.env.step(action)
+        obs = all[0]['observation']
+        reward = all[1]
+        done = all[2]
+        # TODO check if "done" is in 2 or 3
+        info = all[4]
         return obs, reward, done, info
+
 
     def reset(self):
         return self.env.reset()
 
-    def render(self, mode='human'):
-        return self.env.render(mode=mode)
+    def render(self):
+        return self.env.render()
 
     def close(self):
         return self.env.close()
 
     def seed(self, seed=None):
         return self.env.seed(seed=seed)
-    
+
 
 class KitchenImageWrapper(gym.Wrapper):
     '''
