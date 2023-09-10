ddc6827992eeb316e528bbaecd4f5c343f968aa7
Caio Freitas <caio.freitas@usp.br>
2023-09-10 12:24:21 +0200

    add group & seed to wandb

--- train.py
+++ train.py
@@ -27,11 +27,12 @@ os.environ["WANDB_DISABLED"] = "false"
         )
 def train(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
-    log.info("Running train...")
+    log.info("Training policy...")
     # instanciate environment runner from cfg file
     runner = hydra.utils.instantiate(cfg.env_runner)
     # instanciate policy from cfg file
     policy = hydra.utils.instantiate(cfg.policy, env=runner.env)
+    log.info(f"Training policy {policy.__class__.__name__} with seed {cfg.seed} on task {cfg.task.task_name}")
     try:
         if cfg.policy.ckpt_path is not None:
             policy.load_nets(cfg.policy.ckpt_path)
@@ -39,8 +40,9 @@ def train(cfg: DictConfig) -> None:
         log.error("cfg.policy.ckpt_path doesn't exist")
     
     wandb.init(
-        project="se2",
-        name="mlp_overfit",
+        project=cfg.task.task_name,
+        group=policy.__class__.__name__,
+        name=f"seed_{cfg.seed}",
         # track hyperparameters and run metadata
         config={
             "policy": cfg.policy,
@@ -54,7 +56,8 @@ def train(cfg: DictConfig) -> None:
 
     policy.train(dataset=policy.dataset,
                  num_epochs=cfg.num_epochs,
-                 model_path=cfg.model_path)
+                 model_path=cfg.model_path,
+                 seed=cfg.seed)
 
 if __name__ == "__main__":
     train()
