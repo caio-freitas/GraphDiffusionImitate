911e7a5f58d5131ac550eb7ba4dc33c857922b7f
Caio Freitas <43303018+caio-freitas@users.noreply.github.com>
2023-10-06 14:29:08 +0200

    Use joint position for actions instead of OSC (#42)
    * state issue
    
    * add action keys
    
    * add action_keys to datasets
    
    * update robomimic_lowdim_wrapper to allow for joint position control
    
    * display batch shapes in training
    
    * changes after review
    
    ---------
    
    Change controller of RobomimicLowDimWrapper to JOINT_POSITION. This, however, accepts position setpoints relative to the current joint positions.
    
    Workaround:
    
    Override control for joint positions with robots.set_robot_joint_positions(robot_joints). This can result in some unsmoothness especially if the main loop frequency isn't high enough.
    Use JOINT_POSITION control for gripper, since gripper position can't be set as joints are. That means the action going into the controller is filled with zeros in the robot joint entries, and has only values in the gripper entries.
    
    Co-authored-by: Caio Freitas <caio.freitas@usp.br>

--- imitation/env/robomimic_lowdim_wrapper.py
+++ imitation/env/robomimic_lowdim_wrapper.py
@@ -6,6 +6,10 @@ import robosuite as suite
 from robosuite.controllers import load_controller_config
 from robosuite.wrappers.gym_wrapper import GymWrapper
 
+import logging
+from tqdm import tqdm
+
+log = logging.getLogger(__name__)
 
 class RobomimicGymWrapper(GymWrapper):
     # override the default observation keys
@@ -24,7 +28,7 @@ class RobomimicGymWrapper(GymWrapper):
         for key in self.keys:
             if key in obs_dict:
                 if verbose:
-                    print("adding key: {}".format(key))
+                    log.info("adding key: {}".format(key))
                 ob_lst.append(np.array(obs_dict[key]).flatten())
         return np.concatenate(ob_lst)
 
@@ -35,7 +39,7 @@ class RobomimicLowdimWrapper(gym.Env):
                  has_renderer=True,
                  robots=["Panda"],
                  ):
-        controller_config = load_controller_config(default_controller="OSC_POSE")
+        controller_config = load_controller_config(default_controller="JOINT_POSITION") 
         self.robots = [*robots] # gambiarra to make it work with robots list
         keys = [ "robot0_proprio-state", 
                 *[f"robot{i}_proprio-state" for i in range(1, len(self.robots))],
@@ -48,7 +52,7 @@ class RobomimicLowdimWrapper(gym.Env):
                 has_offscreen_renderer=False,  # not needed since not using pixel obs
                 has_renderer=has_renderer,  # make sure we can render to the screen
                 reward_shaping=True,  # use dense rewards
-                control_freq=20,  # control should happen fast enough so that simulation looks smooth
+                control_freq=30,  # control should happen fast enough so that simulation looks smooth
                 horizon=max_steps,  # long horizon so we can sample high rewards
                 controller_configs=controller_config,
             ),
@@ -57,6 +61,7 @@ class RobomimicLowdimWrapper(gym.Env):
         self.env.reset()
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
+        # self._test_routine() # for testing purposes
         
 
     def _robosuite_obs_to_robomimic_obs(self, obs):
@@ -83,7 +88,6 @@ class RobomimicLowdimWrapper(gym.Env):
         
         objects = obs[32*len(self.robots):]
         return [*final_obs, *objects]
-        # return [*robot_joint_cos, *robot_joint_sin, *robot_joint_vel, *eef_pose, *eef_quat, *gripper_pose, *objects]
         
 
     def reset(self):
@@ -92,7 +96,32 @@ class RobomimicLowdimWrapper(gym.Env):
     
 
     def step(self, action):
-        obs, reward, done, sla, info = self.env.step(action)
+        final_action = []
+        current_action = []
+        for i in range(len(self.robots)):
+            j = i*8
+            robot_joints = action[j:j+7]
+            robot_gripper = action[j+7:j+8]
+
+            self.env.robots[i].set_robot_joint_positions(robot_joints) #gripper not included
+
+            obs = self.env._get_observations()
+
+            # use controller only for gripper
+            current_action = [*current_action, *np.zeros(7,), obs[f"robot{i}_gripper_qpos"][-1]]
+            final_action = [*final_action, *np.zeros(7,), *robot_gripper]
+
+
+        final_action = np.array(final_action)
+        current_action = np.array(current_action)
+        
+        q_diff = final_action - current_action
+
+        action = list(q_diff)
+
+        obs, reward, done, _, info = self.env.step(action)
+        self.env.render()        
+
         if reward == 1:
             done = True
             info = {"success": True}
@@ -106,4 +135,19 @@ class RobomimicLowdimWrapper(gym.Env):
 
     def close(self):
         self.env.close()
+
+    def _test_routine(self):
+        '''
+        Test routine to check if the environment is working properly
+        and exemplify degrees of freedom of action space
+        '''
+        self.reset()
+        action = np.zeros(self.action_space.shape)
+        log.info(f"Testing action space {self.action_space}, {self.action_space.shape[0]}")
+        for j in range(self.action_space.shape[0]):
+            log.info(f"Testing DOF {j}")
+            for t in tqdm(range(100)):
+                action[j] = 2*np.sin(3.14*t/50)
+                self.step(action)   
+                self.render()                
 
