455fb46113175dbbd853033f8026f9892eee67ab
Caio Freitas <caio.freitas@usp.br>
2023-10-07 17:49:17 +0200

    joint velocity as actions for robomimic

--- imitation/env/robomimic_lowdim_wrapper.py
+++ imitation/env/robomimic_lowdim_wrapper.py
@@ -39,7 +39,7 @@ class RobomimicLowdimWrapper(gym.Env):
                  has_renderer=True,
                  robots=["Panda"],
                  ):
-        controller_config = load_controller_config(default_controller="JOINT_POSITION") 
+        controller_config = load_controller_config(default_controller="JOINT_VELOCITY") 
         self.robots = [*robots] # gambiarra to make it work with robots list
         keys = [ "robot0_proprio-state", 
                 *[f"robot{i}_proprio-state" for i in range(1, len(self.robots))],
@@ -97,31 +97,17 @@ class RobomimicLowdimWrapper(gym.Env):
 
     def step(self, action):
         final_action = []
-        current_action = []
         for i in range(len(self.robots)):
-            j = i*8
-            robot_joints = action[j:j+7]
-            robot_gripper = action[j+7:j+8]
-
-            self.env.robots[i].set_robot_joint_positions(robot_joints) #gripper not included
-
-            obs = self.env._get_observations()
-
-            # use controller only for gripper
-            current_action = [*current_action, *np.zeros(7,), obs[f"robot{i}_gripper_qpos"][-1]]
-            final_action = [*final_action, *np.zeros(7,), *robot_gripper]
-
-
-        final_action = np.array(final_action)
-        current_action = np.array(current_action)
-        
-        q_diff = final_action - current_action
-
-        action = list(q_diff)
-
-        obs, reward, done, _, info = self.env.step(action)
-        self.env.render()        
-
+            '''
+            Robosuite's action space is composed of 7 joint velocities and 1 gripper velocity, while 
+            in the robomimic datasets, it's composed of 7 joint velocities and 2 gripper velocities (for each "finger").
+            '''
+            j = i*9
+            robot_joint_vel = action[j:j + 7]
+            robot_gripper_vel = action[j + 8] # use only last action for gripper
+            final_action = [*final_action, *robot_joint_vel, robot_gripper_vel]
+        obs, reward, done, _, info = self.env.step(final_action)
+        self.env.render()
         if reward == 1:
             done = True
             info = {"success": True}
