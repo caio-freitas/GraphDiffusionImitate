ece8d57f290e41deb31d5c1dd99fe0b9954ce3eb
Caio Freitas <caio.freitas@usp.br>
2024-01-03 17:44:46 +0100

    remove unused horizon

--- imitation/dataset/robomimic_graph_dataset.py
+++ imitation/dataset/robomimic_graph_dataset.py
@@ -25,9 +25,9 @@ class RobomimicGraphDataset(InMemoryDataset):
         self.mode = mode
         self.action_keys = action_keys
         self.pred_horizon = pred_horizon        #|
-        self.obs_horizon = obs_horizon          #|} FIXED TO 1 in the graph dataset
+        self.obs_horizon = obs_horizon          #| TODO use horizons
         self.action_horizon = action_horizon    #|
-        self.object_state_sizes = object_state_sizes
+        self.object_state_sizes = object_state_sizes # can be taken from https://github.com/ARISE-Initiative/robosuite/tree/master/robosuite/environments/manipulation
         self.object_state_keys = object_state_keys
         self.num_objects = num_objects
         self._processed_dir = dataset_path.replace(".hdf5", "_processed")
@@ -69,16 +69,16 @@ class RobomimicGraphDataset(InMemoryDataset):
     def _get_node_feats(self, data, idx):
         node_feats = []
         if self.mode == "end-effector":
-            node_feats = torch.tensor(data["robot0_eef_pos"][idx - self.obs_horizon:idx][0])
+            node_feats = torch.tensor(data["robot0_eef_pos"][idx - 1:idx][0])
             node_feats = node_feats.reshape(1,3)
         else:
             if self.mode == "task-space":
                 node_feats = []
-                for i in range(idx - self.obs_horizon, idx):
+                for i in range(idx - 1, idx):
                     node_feats.append(self._calculate_joints_positions([*data["robot0_joint_pos"][i], *data["robot0_gripper_qpos"][i]]))
                 node_feats = torch.stack(node_feats)
             elif self.mode == "joint-space":
-                node_feats = torch.tensor(data["robot0_joint_pos"][idx - self.obs_horizon:idx][0])
+                node_feats = torch.tensor(data["robot0_joint_pos"][idx - 1:idx][0])
                 # duplicate dimensions for each joint, to match task-space - since objects are going to be represented in task-space
                 # result should be of shape (7,3)
                 node_feats = node_feats.repeat(3,1).transpose(0,1)
@@ -96,7 +96,7 @@ class RobomimicGraphDataset(InMemoryDataset):
         for object in range(self.num_objects):
             for obj_state in self.object_state_sizes:
                 if obj_state["name"] in self.object_state_keys:
-                    obj_state_tensor[object,i:i + obj_state["size"]] = torch.from_numpy(data["object"][idx - self.obs_horizon:idx][0][i:i + obj_state["size"]])
+                    obj_state_tensor[object,i:i + obj_state["size"]] = torch.from_numpy(data["object"][idx - 1:idx][0][i:i + obj_state["size"]])
                 i += obj_state["size"]
 
         # add dimension for NODE_TYPE, which is 0 for robot and 1 for objects
@@ -135,7 +135,7 @@ class RobomimicGraphDataset(InMemoryDataset):
             episode_length = self.dataset_root[f"data/{key}/obs/object"].shape[0]
             
             for idx in range(episode_length - self.pred_horizon):
-                if idx - self.obs_horizon < 0:
+                if idx - 1 < 0:
                     continue
                 data_raw = self.dataset_root["data"][key]["obs"]
                 node_feats = self._get_node_feats(data_raw, idx)
