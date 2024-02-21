9ddbc9954ecd62f0c7a54a99a9aa5f693e320664
Caio Freitas <caiofreitas@usp.br>
2024-02-21 10:30:09 +0100

    add normalizer

--- imitation/policy/robomimic_lowdim_policy.py
+++ imitation/policy/robomimic_lowdim_policy.py
@@ -13,6 +13,7 @@ from robomimic.algo import algo_factory
 from robomimic.algo.algo import PolicyAlgo
 import robomimic.utils.obs_utils as ObsUtils
 from diffusion_policy.common.robomimic_config_util import get_robomimic_config
+from diffusion_policy.model.common.normalizer import LinearNormalizer
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +21,7 @@ class RobomimicLowdimPolicy(BasePolicy):
     def __init__(self, 
             action_dim, 
             obs_dim,
-            algo_name='bc',
+            algo_name='bc_rnn',
             obs_type='low_dim',
             task_name='square',
             dataset_type='ph',
@@ -54,6 +55,7 @@ class RobomimicLowdimPolicy(BasePolicy):
             )
         self.model = model
         self.nets = model.nets
+        self.normalizer = LinearNormalizer()
         self.obs_key = obs_key
         self.config = config
 
@@ -89,11 +91,13 @@ class RobomimicLowdimPolicy(BasePolicy):
 
     # =========== inference =============
     def get_action(self, obs):
+        obs = self.normalizer['obs'].normalize(obs_dict['obs'])
         if self.model.nets.training:        
             self.model.set_eval()
         obs = torch.tensor([obs], dtype=torch.float32).to(self.device)
         robomimic_obs_dict = {self.obs_key: obs[:,0,:]}
-        action = self.model.get_action(robomimic_obs_dict)
+        naction = self.model.get_action(robomimic_obs_dict)
+        action = self.normalizer['action'].unnormalize(naction)
         # (B, Da)
         return action.cpu().numpy()
     
@@ -101,14 +105,17 @@ class RobomimicLowdimPolicy(BasePolicy):
         self.model.reset()
         
     # =========== training ==============
-    
+    def set_normalizer(self, normalizer: LinearNormalizer):
+        self.normalizer.load_state_dict(normalizer.state_dict())
+
     def train(self, dataset, num_epochs, model_path, seed=0):
         with tqdm(range(num_epochs)) as pbar:
             for epoch in pbar:
                 for batch in self.dataloader:
+                    nbatch = self.normalizer.normalize(batch)
                     robomimic_batch = {
-                        'obs': {self.obs_key: batch['obs']},
-                        'actions': batch['action']
+                        'obs': {self.obs_key: nbatch['obs']},
+                        'actions': nbatch['action']
                     }
                     input_batch = self.model.process_batch_for_training(
                         robomimic_batch)
