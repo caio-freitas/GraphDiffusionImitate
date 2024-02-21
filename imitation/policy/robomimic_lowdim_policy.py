87384dc7d5425a85165313b492a6bab6a74c2382
Caio Freitas <caio.freitas@usp.br>
2024-02-21 14:42:42 +0100

    add robomimic pretrained wrapper

--- imitation/policy/robomimic_lowdim_policy.py
+++ imitation/policy/robomimic_lowdim_policy.py
@@ -11,11 +11,48 @@ from tqdm.auto import tqdm
 
 from robomimic.algo import algo_factory
 from robomimic.algo.algo import PolicyAlgo
+import robomimic.utils.file_utils as FileUtils
 import robomimic.utils.obs_utils as ObsUtils
 from diffusion_policy.common.robomimic_config_util import get_robomimic_config
 from diffusion_policy.model.common.normalizer import LinearNormalizer
 
 log = logging.getLogger(__name__)
+
+class RobomimicPretrainedWrapper:
+    def __init__(self,
+            action_dim,
+            obs_dim,
+            algo_name='bc_rnn',
+            obs_type='low_dim',
+            task_name='square',
+            dataset_type='ph',
+            ckpt_path=None,
+            lr=1e-4
+        ):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # load model from ckpt
+        if ckpt_path is not None:
+            self.load_nets(ckpt_path)
+
+    def load_nets(self, ckpt_path):
+        log.info(f"Loading model from {ckpt_path}")
+        try:
+            policy, ckpt_dict = FileUtils.policy_from_checkpoint(ckpt_path=ckpt_path, device=self.device, verbose=True)
+        except Exception as e:
+            log.error(f"Could not load model from {ckpt_path}. Error: {e}")
+            raise e
+        policy.start_episode()
+        self.policy = policy
+
+    def get_action(self, obs):
+        obs = torch.tensor(obs).to(self.device).squeeze(0)
+        obs_dict = {
+            "robot0_eef_pos": obs[:3],
+            "robot0_eef_quat": obs[3:7],
+            "robot0_gripper_qpos": obs[7:9],
+            "object": obs[9:]
+        }
+        return self.policy(ob=obs_dict)
 
 class RobomimicLowdimPolicy(BasePolicy):
     def __init__(self, 
@@ -88,6 +125,7 @@ class RobomimicLowdimPolicy(BasePolicy):
             self.model.deserialize(torch.load(ckpt_path))
         except Exception as e:
             log.error(f"Could not load model from {ckpt_path}. Error: {e}")
+            raise e
 
     # =========== inference =============
     def get_action(self, obs):
@@ -119,7 +157,7 @@ class RobomimicLowdimPolicy(BasePolicy):
                     input_batch = self.model.process_batch_for_training(
                         robomimic_batch)
                     info = self.model.train_on_batch(
-                        batch=input_batch, epoch=epoch, validate=False)
+                        batch=input_batch, epoch=epoch, validate=True)
                 wandb.log({"epoch": self.global_epoch,
                         "loss": info['losses']['action_loss'],
                         "log_probs": info['losses']['log_probs']})
