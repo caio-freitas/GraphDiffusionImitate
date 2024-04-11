82b4d60c5246654960f96f81c5aab1e7e1e40d7c
Caio Freitas <caiofreitas@usp.br>
2024-04-11 20:39:20 +0200

    separate files for eef and lowdim datasets

--- imitation/dataset/robomimic_eef_dataset.py
+++ imitation/dataset/robomimic_eef_dataset.py
@@ -37,7 +37,7 @@ class RobomimicEefDataset(torch.utils.data.Dataset):
         self.data_at_indices = []
         # if indices file exists, load it
         index_file = dataset_path.replace(".hdf5", f"_indices_{obs_horizon}_{action_horizon}_{pred_horizon}.npy")
-        data_at_indices_file = dataset_path.replace(".hdf5", f"_data_at_indices_{obs_horizon}_{action_horizon}_{pred_horizon}.npy")
+        data_at_indices_file = dataset_path.replace(".hdf5", f"eef__data_at_indices_{obs_horizon}_{action_horizon}_{pred_horizon}.npy")
         if os.path.exists(index_file):
             self.indices = np.load(index_file)
             self.data_at_indices = np.load(data_at_indices_file, allow_pickle=True)
