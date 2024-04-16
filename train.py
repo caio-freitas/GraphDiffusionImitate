98fb00224313e8eb949c595174e3ae712f81c1a2
Caio Freitas <caio.freitas@usp.br>
2024-04-16 19:07:10 +0200

    update tag

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def train(cfg: DictConfig) -> None:
     wandb.init(
         project=policy.__class__.__name__,
         group=cfg.task.task_name,
-        name=f"v1.1.8",
+        name=f"v1.1.9",
         # track hyperparameters and run metadata
         config={
             "policy": cfg.policy,
