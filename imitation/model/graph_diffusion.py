d8be0fa7bb9726924a6bc97415d7a106523e3a8e
Caio Freitas <caio.freitas@usp.br>
2024-04-24 20:53:56 +0200

    parallelize training of ARGD policy

--- imitation/model/graph_diffusion.py
+++ imitation/model/graph_diffusion.py
@@ -134,13 +134,15 @@ class ConditionalARGDenoising(nn.Module):
             Linear(hidden_dim, self.node_feature_dim*self.pred_horizon)
         ).to(self.device)
         
-    def forward(self, x, edge_index, edge_attr, x_coord, film_cond, node_order=None):
+    def forward(self, x, edge_index, edge_attr, x_coord, film_cond, batch=None):
         # make sure x and edge_attr are of type float, for the MLPs
         x = x.float().to(self.device).flatten(start_dim=1)
         edge_attr = edge_attr.float().to(self.device).unsqueeze(-1) # add channel dimension
         edge_index = edge_index.to(self.device)
         film_cond = film_cond.float().to(self.device)
         x_coord = x_coord.float().to(self.device)
+        batch = batch.long().to(self.device) if batch is not None else torch.zeros(x.shape[0], dtype=torch.long).to(self.device)
+        
 
         assert x.shape[0] == x_coord.shape[0], "x and x_coord must have the same length"
 
@@ -159,17 +161,14 @@ class ConditionalARGDenoising(nn.Module):
             h_v = self.layers[l](h_v, edge_index, edge_attr=h_e)
         
         # graph-level embedding, from average pooling layer
-        graph_embedding = global_mean_pool(h_v, batch=None)
+        graph_embedding = global_mean_pool(h_v, batch=batch)
 
         # repeat graph embedding to have the same shape as h_v
-        graph_embedding = graph_embedding.repeat(h_v.shape[0], 1)
+        graph_embedding = graph_embedding[batch]
 
         node_pred = self.node_pred_layer(torch.cat([graph_embedding, h_v], dim=1)) # 2*hidden_dim
-
-        v_t = h_v.shape[0] - 1  # node being masked, this assumes that the masked node is the last node in the graph
         
-        node_pred = node_pred[v_t]
-        node_pred = node_pred.reshape(self.pred_horizon, self.node_feature_dim) # reshape to original shape        
+        node_pred = node_pred.reshape(-1, self.pred_horizon, self.node_feature_dim) # reshape to original shape        
 
         return node_pred, x_v
 
