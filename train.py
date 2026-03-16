1d170ef07a91c21f6534b47b22a280e1d90be97f
Caio Freitas <caio.freitas@usp.br>
2026-03-16 14:45:37 +0100

    Update train.py version

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def train(cfg: DictConfig) -> None:
     wandb.init(
         project=policy.__class__.__name__,
         group=cfg.task.task_name,
-        name=f"v1.2.3 - GDDPM",
+        name=f"v1.2.4 - GDDPM",
         # track hyperparameters and run metadata
         config={
             "policy": cfg.policy,
